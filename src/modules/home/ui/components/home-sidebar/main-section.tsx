7b764e87244bfd8cdbdd5994c76a0889b2189fc3
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-02-09 00:53:34 -0300

    code review and onclick method

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+import Link from "next/link"
+import { useAuth, useClerk } from "@clerk/nextjs"
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react"
 
 import {
@@ -9,7 +10,6 @@ import {
   SidebarMenuButton,
   SidebarMenuItem
 } from "@/components/ui/sidebar"
-import Link from "next/link"
 
 const items = [
   {
@@ -31,6 +31,9 @@ const items = [
 ]
 
 export const MainSection = () => {
+  const clerk = useClerk()
+  const { isSignedIn } = useAuth()
+
   return (
     <SidebarGroup>
       <SidebarGroupContent>
@@ -41,7 +44,12 @@ export const MainSection = () => {
                 tooltip={item.title}
                 asChild
                 isActive={false} // TODO: Change to look at current pathname
-                onClick={() => { }} // TODO: Do something on click
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault()
+                    return clerk.openSignIn()
+                  }
+                }} // TODO: Do something on click
               >
                 <Link href={item.url} className="flex items-center gap-4">
                   <item.icon />
